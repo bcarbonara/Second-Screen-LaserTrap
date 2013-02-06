0d5aae7a63af1e270ede3fb4fed3c88226047e46
unknown <HuangJ@SDL14.ecs.baylor.edu>
2013-02-05 21:51:10 -0600

    added chat

---
+++ Group_Project_Try1/src/com/example/group_project_try1/ChatActivity.java
@@ -0,0 +1,59 @@
+package com.example.group_project_try1;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.support.v4.app.NavUtils;
+
+public class ChatActivity extends Activity {
+	
+	StringBuilder stringBuilder = new StringBuilder();
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_chat);
+		// Show the Up button in the action bar.
+		//getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_chat, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+    
+    public void sendMessage(View view) {
+    	EditText editText = (EditText) findViewById(R.id.edit_message);
+    	if (this.stringBuilder.length() > 0) {
+    		this.stringBuilder.append("\n");
+    	}
+    	this.stringBuilder.append( editText.getText().toString() );
+    	editText.setText("");
+    	TextView textView = (TextView) findViewById(R.id.text_box);
+	    textView.setText(this.stringBuilder.toString());
+    }
+
+}
--- Group_Project_Try1/src/com/example/group_project_try1/Home_Screen_1.java
+++ Group_Project_Try1/src/com/example/group_project_try1/Home_Screen_1.java
@@ -1,8 +1,10 @@
 package com.example.group_project_try1;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class Home_Screen_1 extends Activity {
 
@@ -17,6 +19,13 @@ public class Home_Screen_1 extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_home__screen_1, menu);
         return true;
+    }
+    
+    /** Called when the user clicks the Send button */
+    public void chatNow(View view) {
+        // Do something in response to button
+    	Intent intent = new Intent(this, ChatActivity.class);
+        startActivity(intent);
     }
     
 }
